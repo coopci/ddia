c950930d546aee966c2e302feb778415be136f57
gubo <bo.coopci@gmail.com>
2017-06-16 16:00:47 +0800

    add RabbitmqSubscriber.java

--- ddia-common/src/coopci/ddia/notify/ISubscriber.java
+++ ddia-common/src/coopci/ddia/notify/ISubscriber.java
@@ -1,11 +1,13 @@
 package coopci.ddia.notify;
 
+import java.io.IOException;
+
 // 给gateway用的工具接口，用来接收各个微服务发出的消息，并转发给用户。
 public interface ISubscriber {
 	void config(ISubscriberConf conf);
 	void init();
-	void start();
-	void subscribe(long uid);
-	void onMessageReceived(long uid, String msg);
-	void onBroadcastReceived(long uid, String msg);
+	void start() throws Exception;
+	void subscribe(long uid)  throws Exception;
+	void onMessageReceived(long uid, byte[] msg);
+	void onBroadcastReceived(byte[] msg);
 }
---
+++ ddia-common/src/coopci/ddia/notify/rabbitmq/RabbitmqSubscriber.java
@@ -0,0 +1,107 @@
+package coopci.ddia.notify.rabbitmq;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+import coopci.ddia.notify.ISubscriber;
+import coopci.ddia.notify.ISubscriberConf;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.AMQP;
+
+public class RabbitmqSubscriber implements ISubscriber {
+
+	@Override
+	public void config(ISubscriberConf conf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	String exchangeName = "user.notify";
+	String queueName = "";
+	Channel channel = null;
+	@Override
+	public void start() throws KeyManagementException, NoSuchAlgorithmException, URISyntaxException, IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setUri("amqp://localhost:5672/");
+		factory.setVirtualHost("/");
+		Connection conn = factory.newConnection();
+		channel = conn.createChannel();
+		channel.exchangeDeclare(exchangeName, "topic", true);
+		queueName = channel.queueDeclare().getQueue();
+		
+		boolean autoAck = true;
+		channel.basicConsume(queueName, autoAck, "myConsumerTag",
+			     new DefaultConsumer(channel) {
+			         @Override
+			         public void handleDelivery(String consumerTag,
+			                                    Envelope envelope,
+			                                    AMQP.BasicProperties properties,
+			                                    byte[] body)
+			             throws IOException
+			         {
+			             String routingKey = envelope.getRoutingKey();
+			             String contentType = properties.getContentType();
+			             long deliveryTag = envelope.getDeliveryTag();
+			             if (routingKey.equals("broadcast")) {
+			            	 onBroadcastReceived(body);
+			             } else if (routingKey.startsWith("uid=")) {
+			            	 long uid = Long.parseLong(routingKey.replace("uid=", ""));
+			            	 onMessageReceived(uid, body);
+			             }
+			         }
+			     });
+		channel.queueBind(this.queueName, exchangeName, "broadcast");
+		
+	}
+
+	@Override
+	public void subscribe(long uid) throws IOException {
+		if (this.channel == null)
+			return;
+		String routingKey = "uid=" + uid;
+		channel.queueBind(this.queueName, exchangeName, routingKey);
+	}
+
+	@Override
+	public void onMessageReceived(long uid, byte[] msg) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("onMessageReceived(");
+		sb.append(uid);
+		sb.append(", \"");
+		sb.append(new String(msg));
+		sb.append("\")");
+		System.out.println(sb.toString());
+	}
+
+	@Override
+	public void onBroadcastReceived(byte[] msg) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("onBroadcastReceived(\"");
+		sb.append(new String(msg));
+		sb.append("\")");
+		System.out.println(sb.toString());
+	}
+
+	
+	public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException, URISyntaxException, IOException, TimeoutException {
+		RabbitmqSubscriber sub = new RabbitmqSubscriber();
+		sub.start();
+		
+		
+		return;
+	}
+}
