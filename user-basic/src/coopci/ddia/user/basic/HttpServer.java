08a76303ba24b0aa59be58a6c3a0e715ad4400aa
gubo <bo.coopci@gmail.com>
2017-06-27 11:12:50 +0800

    给user-basic的HttpServer增加start()方法。

--- user-basic/src/coopci/ddia/user/basic/HttpServer.java
+++ user-basic/src/coopci/ddia/user/basic/HttpServer.java
@@ -23,7 +23,9 @@ import coopci.ddia.user.basic.handlers.sms.LoginSubmitVcodeHandler;
 
 public class HttpServer {
 	public static int listenPort = 8888;
-	public static void main(String[] argv) throws Exception {
+	
+	public void start() throws Exception {
+		
 		Engine engine = new Engine();
 		engine.init();
 		
@@ -101,11 +103,23 @@ public class HttpServer {
 			
 			server.addListener(nl);
 		    server.start();
-		    System.out.println("Press any key to stop the server...");
-		    System.in.read();
 		} catch (Exception e) {
 		    System.err.println(e);
 		}
+		return;
+		
+	}
+	public static void main(String[] argv) throws Exception {
+		HttpServer server = new HttpServer();
+		
+		try {
+			server.start();
+		} catch (Exception e) {
+		    System.err.println(e);
+		}
+
+	    System.out.println("Press any key to stop the server...");
+	    System.in.read();
 		return;
 	}
 }
