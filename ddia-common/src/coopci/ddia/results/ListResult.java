ec350de6410f16f581ac364ed6cee909a3da772b
gubo <bo.coopci@gmail.com>
2017-07-13 10:59:59 +0800

    ListResult 增加total字段。

--- ddia-common/src/coopci/ddia/results/ListResult.java
+++ ddia-common/src/coopci/ddia/results/ListResult.java
@@ -10,8 +10,12 @@ import coopci.ddia.Result;
 
 public class ListResult extends Result {
 	
-	
+	// 这个data里面只有本次返回的实际数据。
 	public List<KVItem> data = new LinkedList<KVItem>();
+	
+	// 如过total>0， 则表示"数据库"里一共有total条符合条件的数据，data里面是本"页"的数据。
+	// 本"页"的筛选条件 和 查询的筛选条件 的语义由各个API自己定。
+	public int total = -1;
 	
 	public void add(KVItem kvitem) {
 		this.data.add(kvitem);
