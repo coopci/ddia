9b3d510c2014db1f9346f62fab5cdc08a8737f51
gubo <bo.coopci@gmail.com>
2017-07-24 17:53:02 +0800

    ModifyPassword.java 增加参数和help。

--- ddia-launchers/src/coopci/ddia/tools/ModifyPassword.java
+++ ddia-launchers/src/coopci/ddia/tools/ModifyPassword.java
@@ -1,16 +1,30 @@
 package coopci.ddia.tools;
 
+
+// java coopci.ddia.tools.ModifyPassword admin q298tgy9
 public class ModifyPassword {
 
+	public static void help() throws Exception {
+		
+		System.out.println("This command takes exactly 2 parameters: ");
+		System.out.println("java coopci.ddia.tools.ModifyPassword $nickname $newpassword");
+		
+	}
 	public static void main(String[] argv) throws Exception {
-		String nickname = "admin";
-		String newPassword = "123qwe9";
+		
+		if (argv.length < 2) {
+			
+			help();
+			System.exit(1);
+		}
+		String nickname = argv[0];
+		String newPassword = argv[1];
 		coopci.ddia.user.basic.Engine engine = new coopci.ddia.user.basic.Engine();
 		engine.init();
 		
 		engine.addUser(nickname, newPassword, null);
 		
 		engine.modifyPassword(nickname, newPassword);
-		
+		System.out.println("OK");
 	}
 }
